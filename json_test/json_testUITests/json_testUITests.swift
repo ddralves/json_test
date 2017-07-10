7ed8986905f7f4c4b2bbda5b540304a8afde32bc
Dino Alves <dino@futre.co.za>
2017-07-10 22:46:50 +0200

    WIP

---
+++ json_test/json_testUITests/json_testUITests.swift
@@ -0,0 +1,41 @@
+//
+//  json_testUITests.swift
+//  json_testUITests
+//
+//  Created by Dino Alves on 2017/07/10.
+//  Copyright © 2017 Dino Alves. All rights reserved.
+//
+
+import XCTest
+
+class json_testUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        let app = XCUIApplication()
+        
+        setupSnapshot(app)
+        
+        app.launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+        let app = XCUIApplication()
+        snapshot("Test")
+        app.buttons["Search for e-mail"].tap()
+        app.alerts["Something is wrong!"].buttons["Try Again"].tap()
+        app.buttons["Display User's Names"].tap()
+        app.alerts["Fetched Users"].buttons["OK"].tap()
+        app.buttons["Get Samantha's E-mail"].tap()
+        snapshot("Test1")
+        app.alerts["E-mail for Samantha found!"].buttons["OK"].tap()
+        
+    }
+    
+}
